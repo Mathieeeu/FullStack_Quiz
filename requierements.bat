209424067dfe6b4175caa89e7f26e200d96e2543
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-10-31 20:24:32 +0100

    dernier ajustement

---
+++ requierements.bat
@@ -0,0 +1,15 @@
+@echo off
+
+@REM Attention ça marche pas encore ça mais les commandes peuvent être tapées à la main :D
+
+echo Installing npm packages...
+
+cd backend/
+npm install express
+npm install mongoose
+npm install body-parser
+npm install cors
+npm install dotenv
+
+echo All npm packages have been installed, this window will close in 5 seconds.
+timeout /t 5

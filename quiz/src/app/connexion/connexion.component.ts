da4761f8575f35ed834f2492baafa1a005ad984d
Louna Camas <lounacamas@gmail.com>
2024-10-31 09:52:41 +0100

    Bouton valider

--- quiz/src/app/connexion/connexion.component.ts
+++ quiz/src/app/connexion/connexion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './connexion.component.css'
 })
 export class ConnexionComponent {
+  constructor(private router: Router) {}
+
+  // Méthode appelée au clic du bouton
+  goToAdmin(): void {
+    this.router.navigate(['/admin']);
+
+  }
 
 }

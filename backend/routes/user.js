a8c68c568480e51960ad2ca904a089cedc131fb4
Louna Camas <lounacamas@gmail.com>
2024-11-14 16:28:34 +0100

    API user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -14,16 +14,91 @@ module.exports = (collection) => {
     });
 
     // Route pour créer un nouvel utilisateur
-
+    router.post('/add', async (req, res) => {
+        const user = {
+            userLogin: req.body.login,
+            userPW: req.body.password,
+            Superuser: req.body.superuser
+        };
+        
+        try {
+            // Recherche si login déjà dans la bd
+            const result = await collection.findOne({ username: user.userLogin});
+            if (result != null){
+                res.status(409).send({ message: "User name already used" });
+            }
+            else{
+                const result1 = await collection.insertOne(user);
+                res.send("User added");
+                console.log(`User ${user.userLogin} added`);
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 
     // Route pour modifier les informations d'un utilisateur
-
+    //Rajouter vérification mdp !!!!
+    router.post('/edit', async (req, res) => {
+        const user = {
+            userLogin: req.body.login,
+            userPW: req.body.password,
+            Superuser: req.body.superuser
+        };
+        
+        try {
+             // Recherche si login déjà dans la bd
+             const result = await collection.findOne({ username: user.userLogin});
+             if (result === null){
+                 res.status(404).send({ message: "User not found" });
+             }
+             else{
+                const result = await collection.updateOne({ username: user.userLogin }, { $set: { password: user.userPW, Superuser: user.Superuser } });
+                console.log(`User ${user.userLogin} edited: `);
+                res.send("User edited");
+             }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 
     // Route pour supprimer un utilisateur
+    //Rajouter vérification mdp !!!!
+    router.post('/delete', async (req, res) => {
+        const userLogin = req.body.login;
+        const userPW = req.body.password;
 
+        try {
+            const deleteResult = await collection.deleteOne({userLogin});
+            if (deleteResult.deletedCount === 0) {
+                return res.status(404).send({ message: "User not found" });
+            }
+            res.send("User deleted");
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 
     // Route pour permettre à un utilisateur de se connecter
+    router.post('/login', async (req, res) => {
+        const user = {
+            userLogin: req.body.login,
+            userPW: req.body.password
+        };
 
+        // Recherche de l'utilisateur
+        try {
+            const result = await collection.findOne({ username: user.userLogin, password: user.userPW});
+            if(!result){
+                res.send(false)
+            }
+            else{
+                res.send(true)
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
 
     return router;
 };

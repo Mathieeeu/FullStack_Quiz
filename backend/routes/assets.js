35a1530232ac17523b47d96ce8bfb59ebf364e08
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-12-28 14:29:45 +0100

    requete api pour voir la répartition des questions par thème ! 👓

--- backend/routes/assets.js
+++ backend/routes/assets.js
@@ -1,6 +1,7 @@
 // backend/routes/assets.js
 
 const express = require('express');
+const axios = require('axios');
 
 module.exports = (collection) => {
     const router = express.Router();
@@ -27,12 +28,53 @@ module.exports = (collection) => {
         }
     });
 
-    // Route pour récupérer les thèmes
-    router.get('/themes', async(req,res) => {
+    // Route pour récupérer les thèmes (et générer des informations dessus)
+    router.get('/themes', async (req, res) => {
         try {
             const result = await collection.find().toArray();
             const themes = result[0].themes;
-            res.json({ themes: themes });
+    
+            // Récupérer le nombre total de questions via une requête à l'API
+            const requeteAPI = 'http://127.0.0.1:3000/api/question/nb/';
+            const response = await axios.get(`${requeteAPI}*`);
+            const totalQuestions = response.data.nbQuestions;
+
+            // Ajouter les informations supplémentaires à chaque thème
+            const themesWithDetails = await Promise.all(themes.map(async theme => {
+                const themeResponse = await axios.get(`${requeteAPI}theme=${theme}`);
+                const numberOfQuestions = themeResponse.data.nbQuestions;
+                return {
+                    name: theme,
+                    // backgroundColor: theme.backgroundColor,
+                    // textColor: theme.textColor,
+                    numberOfQuestions: numberOfQuestions,
+                    percentageOfTotalQuestions: parseFloat((numberOfQuestions / totalQuestions).toFixed(2))
+                };
+            }));
+
+            // Récupérer les questions ayant des thèmes qui ne sont pas dans la db (et indiquer les noms de ces thèmes)
+            const allQuestionsResponse = await axios.get('http://127.0.0.1:3000/api/question/*');
+            const allQuestions = allQuestionsResponse.data;
+            const undefinedQuestions = allQuestions.filter(question => !themes.includes(question.themeText));
+            const undefinedNumberOfQuestions = undefinedQuestions.length;
+
+            const undefinedTheme = {
+                numberOfQuestions: undefinedNumberOfQuestions,
+                percentageOfTotalQuestions: parseFloat((undefinedNumberOfQuestions / totalQuestions).toFixed(2)),
+                themes: undefinedQuestions.map(question => question.themeText).filter((value, index, self) => self.indexOf(value) === index) // Supprimer les doublons
+            };
+
+            // Ajouter l'objet * à la liste des thèmes
+            const allThemes = {
+                '*' : {
+                    numberOfThemes: themes.length,
+                    numberOfQuestions: totalQuestions,
+                    percentageOfTotalQuestions: 1.0
+                },
+                themes: themesWithDetails,
+                'undefined': undefinedTheme
+            };
+            res.json(allThemes);
 
         } catch (error) {
             res.status(500).send(error);
